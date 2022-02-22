e10462a0bde197493f689eeeebdc10f0e1e3387d
ahmedsalahos <ahmedfci2014@gmail.com>
2022-02-22 11:56:52 +0200

    Language Module - Create migration -Create crud operations -Create api docs -Create routes

---
+++ app/Repositories/LanguageRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Language;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class LanguageRepository
+ * @package App\Repositories
+ * @version February 22, 2022, 9:56 am UTC
+*/
+
+class LanguageRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Language::class;
+    }
+}
