c99a90febac17159bf279a2adb7a2cdb256b7e5f
ahmedsalahos <ahmedfci2014@gmail.com>
2022-02-27 14:36:27 +0200

    Tables Admission  - Crud operations  - Migration  - Api Docs

--- routes/api.php
+++ routes/api.php
@@ -100,5 +100,6 @@ use Illuminate\Support\Facades\Route;
 
         Route::resource('marital_statuses', App\Http\Controllers\API\MaritalStatusAPIController::class);
 
+        Route::resource('admissions', App\Http\Controllers\API\AdmissionAPIController::class);
     });
 // });
