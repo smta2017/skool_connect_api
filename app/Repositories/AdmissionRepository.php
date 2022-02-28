851064b5d66ef6060d9562b0eec4351f58080d16
ahmedsalahos <ahmedfci2014@gmail.com>
2022-02-28 13:14:39 +0200

    new updates

--- app/Repositories/AdmissionRepository.php
+++ app/Repositories/AdmissionRepository.php
@@ -50,22 +50,16 @@ class AdmissionRepository extends BaseRepository
         $model = null;
         DB::beginTransaction();
         try {
-            $studentRepo = new StudentRepository($this->app);
-            $parentRepo = new ParentRepository($this->app);
-            $input['student'] = array('first_name_ar'=>'a','first_name_en'=>'b');
-            $student = $studentRepo->create($input['student']);
-            $parent1 = $parentRepo->create($input['parent1']);
-            $parent2 = $parentRepo->create($input['parent2']);
-            $input['student_id'] = $student['id'];
-            $input['parent1_id'] = $parent1['id'];
-            $input['parent2_id'] = $parent2['id'];
             $model = $this->model->newInstance($input);
+            $model->Student()->create($input['student']);
+            $model->Parent1()->create($input['parent1']);
+            $model->Parent2()->create($input['parent2']);
             $model->save();
             DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
         }
-        return $student;
+        return $model;
     }
 
 }
